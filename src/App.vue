865f50c889461301c4bf49d46a9f715ee529e021
ahlem-fahem <hallouma.fahem1@gmail.com>
2022-03-14 10:31:46 +0100

    Avancer sur les fichier Home et App.vue pour créer la page Login :construction_worker:

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="background-image: url('./assets/bg2.jpg');"
+       class="flex
+              flex-col pt-10
+              items-center
+              h-screen"
+  >
+  </div>
+</template>
+
+<style>
+body {
+  background-image: url('./assets/bg2.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position:100% 100%;
+  background-size: cover;
+}
+</style>
+
+<script>
+export default {
+}
+</script>
