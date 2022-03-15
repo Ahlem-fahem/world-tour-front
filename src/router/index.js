17b7fa90c5596c53477423e4b0aa7592690a0ad2
ahlem-fahem <hallouma.fahem1@gmail.com>
2022-03-15 15:56:32 +0100

    Ajouter Route Vers HomePage :heavy_plus_sign:

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '/src/Pages/Authentication/Login.vue'
 import Register from '/src/Pages/Authentication/Register.vue'
+import Home from '/src/Pages/Home/HomePage.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: Register,
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
   }
 ]
 
